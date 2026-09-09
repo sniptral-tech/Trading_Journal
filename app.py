6e3da056439f71542c3bf9c48ba9acb8e5034f1b
sniptral-tech <sniptral@gmail.com>
2026-09-09 05:26:14 +0000

    Clean up whitespace in app.py
    Remove unnecessary newline before loading style.css.

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-
 # Chargement du fichier style.css
 with open("style.css", "r", encoding="utf-8") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
